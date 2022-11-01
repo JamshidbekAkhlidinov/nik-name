f96ec693bc0bcae50908eda87145cfccd955ffe3
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2022-11-01 21:36:41 +0500

    updated

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include_once "./src/NikName.php";
 use jamshidbekakhlidinov\NikName;
 
 $text = new NikName();
-$text->setText("Jamshidbek");
+$text->setText("Jamshidbek Axlidinov");
 
 for ($i=1; $i <=20 ; $i++) 
     echo $text->getText($i)."<br>";
